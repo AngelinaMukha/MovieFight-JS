1a69ca6ab6d94a7c2709d9d7e5e04f32734e16fb
Anhelina Mukha <s20987@pjwstk.edu.pl>
2021-07-30 12:25:44 +0300

    search input sends requests

--- index.js
+++ index.js
@@ -1,13 +1,54 @@
-const fetchData = async()=>{
-    const response = await axios.get('http://www.omdbapi.com/', {
+const fetchData=async(searchData)=>{
+    const response = await axios.get('http://www.omdbapi.com/',{
         params:{
             apikey: '4f3af505',
-            i: "tt0848228"
+            s: searchData
         }
-    });
-
-
-    console.log(response.data);
+    })
+}
+const input = document.querySelector('input');
+const onInput=(event)=>{
+    fetchData(event.target.value);
 };
 
-fetchData();
+input.addEventListener('input',onInput);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const fetchData = async(searchTerm)=>{
+//     const response = await axios.get('http://www.omdbapi.com/', {
+//         params:{
+//             apikey: '4f3af505',
+//             s: searchTerm
+//         }
+//     });
+
+//     console.log(response.data);
+// };
+// const input = document.querySelector('input');
+
+// let timeoutId;
+// const onInput = (event) => {
+//     if(timeoutId){
+//         clearTimeout(timeoutId);
+//     }
+//     timeoutId=setTimeout(()=>{
+//         fetchData(event.target.value);
+//     },1000)
+    
+// };
+// input.addEventListener('input', onInput);
+
+
